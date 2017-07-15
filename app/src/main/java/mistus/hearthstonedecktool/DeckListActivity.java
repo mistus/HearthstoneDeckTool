0fb42d49d7a38b96b341747ba81483a21adc1060
mistus <act9357@yahoo.co.jp>
2017-07-16 01:09:13 +0900

    new project first commit

---
+++ app/src/main/java/mistus/hearthstonedecktool/DeckListActivity.java
@@ -0,0 +1,13 @@
+package mistus.hearthstonedecktool;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DeckListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_deck_list);
+    }
+}
