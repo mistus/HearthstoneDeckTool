c367470c0af55b9aa203383e5a3835afd9c61e97
mistus <act9357@yahoo.co.jp>
2017-08-02 01:44:22 +0900

    fix_spinner_value_is_default

--- app/src/main/java/mistus/hearthstonedecktool/Activity/DeckEditActivity.java
+++ app/src/main/java/mistus/hearthstonedecktool/Activity/DeckEditActivity.java
@@ -13,6 +13,7 @@ import android.util.Log;
 import java.util.ArrayList;
 
 import mistus.hearthstonedecktool.CardView.Card.CardRecycleViewAdapter;
+import mistus.hearthstonedecktool.Common.Common;
 import mistus.hearthstonedecktool.Database.DeckToolDatabaseHelper;
 import mistus.hearthstonedecktool.R;
 
@@ -32,21 +33,26 @@ public class DeckEditActivity extends AppCompatActivity {
         String careerName = intent.getStringExtra("careerName");
         String TypeName = intent.getStringExtra("TypeName");
 
+        int careerNameId = _getCareerNameId(careerName);
+
+        String SQL = "select * from card where Job = "+ careerNameId;
          Log.e("deckName List = ",deckName);
          Log.e("careerName = ",careerName);
          Log.e("TypeName = ",TypeName);
-        
-        _createCards();
+
+        Log.e("SQL = ",SQL);
+        _createCards(SQL);
 
     }
 
-    private void _createCards(){
+    private void _createCards(String SQL){
         cardRecyclerView = (RecyclerView)findViewById(R.id.card_recycler);
 
         SQLiteOpenHelper DeckToolDatabaseHelper = new DeckToolDatabaseHelper(this);
         SQLiteDatabase DB = DeckToolDatabaseHelper.getReadableDatabase();
         //TODO change SQL
-        Cursor cursor = DB.rawQuery("select * from card",null);
+//        Cursor cursor = DB.rawQuery("select * from card",null);
+        Cursor cursor = DB.rawQuery(SQL, null);
         int count = cursor.getCount();
         int[] ids = new int[count] ;
         String[] names = new String [count];
@@ -55,7 +61,7 @@ public class DeckEditActivity extends AppCompatActivity {
             cursor.moveToNext();
             ids[i] = cursor.getInt(0);
             names[i] = cursor.getString(1);
-            Log.e("name", cursor.getString(1));
+//            Log.e("name", cursor.getString(1));
         }
 
         cursor.close();
@@ -66,5 +72,31 @@ public class DeckEditActivity extends AppCompatActivity {
         LinearLayoutManager layoutManager = new LinearLayoutManager(this);
         cardRecyclerView.setLayoutManager(layoutManager);
         cardRecyclerView.setAdapter(adapter);
+    }
+
+    private int _getCareerNameId(String CareerName){
+        switch (CareerName){
+            case "戰士":
+                return Common.warrior;
+            case "薩滿":
+                return Common.shaman;
+            case "盜賊":
+                return Common.rogue;
+            case "聖騎士":
+                return Common.paladin;
+            case "獵人":
+                return Common.hunter;
+            case "德魯伊":
+                return Common.druid;
+            case "術士":
+                return Common.warlock;
+            case "法師":
+                return Common.mage;
+            case "牧師":
+                return Common.priest;
+            default:
+                return 0;
+        }
+
     }
 }
--- app/src/main/java/mistus/hearthstonedecktool/FragmentDialog/choose_career_fragment_dialog.java
+++ app/src/main/java/mistus/hearthstonedecktool/FragmentDialog/choose_career_fragment_dialog.java
@@ -6,24 +6,30 @@ import android.app.DialogFragment;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.TextView;
-
 import mistus.hearthstonedecktool.Activity.DeckEditActivity;
 import mistus.hearthstonedecktool.R;
 
 public class choose_career_fragment_dialog extends DialogFragment{
 
+    private Spinner careerNameSpinner;
+    private TextView deckNameTextView;
+    private RadioGroup deckTypeRadioGroup;
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         LayoutInflater inflater = getActivity().getLayoutInflater();
         View view  = inflater.inflate(R.layout.dialogview_choose_career, null);
+
+        careerNameSpinner = (Spinner)view.findViewById(R.id.careerName);
+        deckNameTextView = (TextView) view.findViewById(R.id.deckName);
+        deckTypeRadioGroup = (RadioGroup)view.findViewById(R.id.deckTypeRadioGroup);
+
 
         builder.setView(view)
                 .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
@@ -31,18 +37,12 @@ public class choose_career_fragment_dialog extends DialogFragment{
                     public void onClick(DialogInterface dialog, int id) {
                         LayoutInflater inflater = getActivity().getLayoutInflater();
                         View view  = inflater.inflate(R.layout.dialogview_choose_career, null);
-                        TextView deckNameTextView = (TextView) view.findViewById(R.id.deckName);
+
                         String deckName = deckNameTextView.getText().toString();
-                        Spinner careerNameSpinner = (Spinner)view.findViewById(R.id.careerName);
                         String careerName = toString().valueOf(careerNameSpinner.getSelectedItem());
-                        RadioGroup deckTypeRadioGroup = (RadioGroup)view.findViewById(R.id.deckTypeRadioGroup);
                         int radioButtonID = deckTypeRadioGroup.getCheckedRadioButtonId();
                         RadioButton radioButton = (RadioButton)deckTypeRadioGroup.findViewById(radioButtonID);
                         String TypeName = radioButton.getText().toString();
-
-//                        Log.e("deckName = ",deckName);
-//                        Log.e("careerName = ",careerName);
-//                        Log.e("TypeName = ",TypeName);
 
                         Intent intent = new Intent();
                         intent.setClass(getActivity(), DeckEditActivity.class);
@@ -57,7 +57,6 @@ public class choose_career_fragment_dialog extends DialogFragment{
                 })
                 .setNegativeButton("No", new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
-
                     }
                 });
         return builder.create();
